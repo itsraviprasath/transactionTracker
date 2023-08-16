a369b0ca1a08425bf986b2547a0b4b08c49aa03d
Raviprasath R <raviprasath.in@gmail.com>
2023-08-16 10:41:21 +0530

    Routing add

---
+++ frontend/src/components/LoginRegister.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginRegister = () => {
+  return (
+    <>
+      <section className="login-register">
+        <div></div>
+      </section>
+    </>
+  );
+};
+
+export default LoginRegister;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoginRegister } from "../routes";
+
+const Login = () => {
+  return (
+    <>
+      <LoginRegister />
+    </>
+  );
+};
+
+export default Login;
