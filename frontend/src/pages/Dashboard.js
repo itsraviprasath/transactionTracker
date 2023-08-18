b29d1fac0035ab67a840e19b6371fbc5b1baf350
Raviprasath R <raviprasath.in@gmail.com>
2023-08-18 10:06:34 +0530

    User page and Dashboard page update

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -5,7 +5,29 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-      {/* <Card /> */}
+      <main className="dashboard">
+        <h2>Hi Raviprasath, Welcome back!</h2>
+        <div className="grid-container">
+          <Card title="Bending Amount" value="7000" />
+          <Card title="Paid Amount" value="3000" />
+          <Card title="Total Amount" value="10000" />
+        </div>
+
+        <div className="grid-container">
+          <Card title="Active Loans" value="1" />
+          <Card title="Closed Loans" value="0" />
+          <Card title="All Loans" value="1" />
+        </div>
+
+        <div className="grid-container">
+          <div className="grid-item">
+            <h3>Loan Tracker</h3>
+          </div>
+          <div className="grid-item">
+            <h3>Upcoming Payments</h3>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
